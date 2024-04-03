998b4d5c0a8251f0cfee40fe47e5c387e27a66b1
jsperling23 <95095370+jsperling23@users.noreply.github.com>
2024-04-03 15:09:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ import os
 
 app = Flask(__name__)
 app.config["MYSQL_HOST"] = "classmysql.engr.oregonstate.edu"
-app.config["MYSQL_USER"] = "cs340_sperlijo"
-app.config["MYSQL_PASSWORD"] = "2777"
-app.config["MYSQL_DB"] = "cs340_sperlijo"
+app.config["MYSQL_USER"] = "####"
+app.config["MYSQL_PASSWORD"] = "####"
+app.config["MYSQL_DB"] = "####"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
